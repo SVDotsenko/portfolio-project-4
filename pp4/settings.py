a24347d3704b79b67fbdec1a3182c78ed455b4e1
Serhii Dotsenko <serdotsenko@gmail.com>
2024-03-16 21:26:34 +0000

    deploy on heroku

--- pp4/settings.py
+++ pp4/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k@jg(+2gh$p$10+h88vjy(uj8u#2y79933$+dp$x9zw$m_r3xl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".codeanyapp.com", ".herokuapp.com"]
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'pp4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
