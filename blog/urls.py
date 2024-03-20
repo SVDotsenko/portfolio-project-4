5d3f5d03e2e0828d3b732ec521d68de37611f361
Serhii Dotsenko <serdotsenko@gmail.com>
2024-03-20 15:12:37 +0000

    Edit, update page was added

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
+    path('<int:id>/', views.book_edit, name="book_edit"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Book
 
@@ -5,3 +6,10 @@ from .models import Book
 class BookList(generic.ListView):
     queryset = Book.objects.all()
     template_name = "blog/index.html"
+
+
+def book_edit(request, id):
+
+    queryset = Book.objects.filter(id=id)
+    book = get_object_or_404(queryset, id=id)
+    return render(request, "blog/create-update.html", {"book": book},)
