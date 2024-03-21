789ef2bdd4f2c481ba56b71549798619d9b95481
Serhii Dotsenko <serdotsenko@gmail.com>
2024-03-21 09:43:36 +0000

    The select in form linked with resource

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Book
+from .models import Book, Author
 
 
 class BookList(generic.ListView):
@@ -9,7 +9,14 @@ class BookList(generic.ListView):
 
 
 def book_edit(request, id):
-
+    authors = Author.objects.all()
     queryset = Book.objects.filter(id=id)
     book = get_object_or_404(queryset, id=id)
-    return render(request, "blog/create-update.html", {"book": book},)
+    return render(
+        request,
+        "blog/create-update.html",
+        {
+            "book": book,
+            "authors": authors
+        },
+    )
