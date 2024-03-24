4eaa27fd3e2ce22e679cb77a203b3b2dbe67e061
Serhii Dotsenko <serdotsenko@gmail.com>
2024-03-24 15:40:30 +0000

    refactoring: united create and update in one function

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
-    path('<int:book_id>/', views.update_book, name="update_book"),
+    path('/', views.create_update_book, name='create_book'),
+    path('<int:book_id>/', views.create_update_book, name="update_book"),
     path('add-book/', views.AddBookView.as_view(), name="add_book"),
-    path('/', views.create_book, name='create_book'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,10 +16,14 @@ class AddBookView(generic.ListView):
     template_name = "blog/create-book.html"
 
 
-def update_book(request, book_id):
+def create_update_book(request, book_id=-1):
     if request.method == "POST":
-        book = get_object_or_404(Book, id=book_id)
-        book_form = AddBookForm(data=request.POST, instance=book)
+        if book_id < 0:
+            book_form = AddBookForm(data=request.POST)
+        else:
+            book = get_object_or_404(Book, id=book_id)
+            book_form = AddBookForm(data=request.POST, instance=book)
+
         if book_form.is_valid():
             book = book_form.save(commit=False)
             book.title = request.POST["title"]
@@ -32,6 +36,7 @@ def update_book(request, book_id):
     authors = Author.objects.all()
     queryset = Book.objects.filter(id=book_id)
     book = get_object_or_404(queryset, id=book_id)
+
     return render(
         request,
         "blog/update-book.html",
@@ -40,16 +45,3 @@ def update_book(request, book_id):
             "authors": authors
         },
     )
-
-
-def create_book(request):
-    if request.method == "POST":
-        book_form = AddBookForm(data=request.POST)
-        if book_form.is_valid():
-            book = book_form.save(commit=False)
-            book.title = request.POST["title"]
-            book.author = Author.objects.filter(id=request.POST["author"])[0]
-            book.reader = request.user
-            book.save()
-
-    return HttpResponseRedirect(reverse('home'))
