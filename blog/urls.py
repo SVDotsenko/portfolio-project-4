9c6a89aa84a0778eff74f773d1991e7689eb6709
Serhii Dotsenko <serdotsenko@gmail.com>
2024-03-24 07:56:06 +0000

    Edit (update) mechanism implemented for book

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
-    path('<int:book_id>/', views.book_edit, name="book_edit"),
+    path('<int:book_id>/', views.update_book, name="update_book"),
     path('add-book/', views.AddBookView.as_view(), name="add_book"),
     path('/', views.create_book, name='create_book'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from django.http import HttpResponse, HttpResponseRedirect
-
 from .forms import AddBookForm
 from .models import Book, Author
 
@@ -11,24 +10,36 @@ class BookList(generic.ListView):
     template_name = "blog/index.html"
 
 
-def book_edit(request, book_id):
+class AddBookView(generic.ListView):
+    model = Book
+    queryset = Author.objects.all()
+    template_name = "blog/create-book.html"
+
+
+def update_book(request, book_id):
+    if request.method == "POST":
+        book = get_object_or_404(Book, id=book_id)
+        book_form = AddBookForm(data=request.POST, instance=book)
+        if book_form.is_valid():
+            book = book_form.save(commit=False)
+            book.title = request.POST["title"]
+            book.author = Author.objects.filter(id=request.POST["author"])[0]
+            book.reader = request.user
+            book.save()
+
+        return HttpResponseRedirect(reverse('home'))
+
     authors = Author.objects.all()
     queryset = Book.objects.filter(id=book_id)
     book = get_object_or_404(queryset, id=book_id)
     return render(
         request,
-        "blog/create-update.html",
+        "blog/update-book.html",
         {
             "book": book,
             "authors": authors
         },
     )
-
-
-class AddBookView(generic.ListView):
-    model = Book
-    queryset = Author.objects.all()
-    template_name = "blog/create-book.html"
 
 
 def create_book(request):
