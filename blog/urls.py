5c8c97457647bcfe78b0245f3418538d9b18ade6
Serhii Dotsenko <serdotsenko@gmail.com>
2024-03-26 12:44:02 +0000

    refactoring: function views transformed to class view

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
-    path('add-book/', views.AddBookView.as_view(), name="add_book"),
-    path('/', views.create_update_book, name='create_book'),
-    path('<int:book_id>/', views.create_update_book, name="update_book"),
-    path('<int:book_id>/delete', views.delete_book, name="delete_book"),
+    path('book/', views.BookDetail.as_view(), name="add_book"),
+    path('book/<int:book_id>/', views.BookDetail.as_view(), name="update_book"),
+    path('book/<int:book_id>/delete', views.BookList.as_view(),
+         name="delete_book"),
+
+    path('authors/', views.AuthorList.as_view(), name='authors'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,29 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.views import View
 from .forms import AddBookForm
 from .models import Book, Author
 
 
-class BookList(generic.ListView):
-    queryset = Book.objects.all()
-    template_name = "blog/index.html"
+class BookList(View):
+    def get(self, request, book_id=-1):
+        books = Book.objects.all()
+        if book_id < 0:
+            context = {'object_list': books}
+            return render(request, "blog/book/index.html", context)
+        get_object_or_404(books, id=book_id).delete()
+        return redirect('home')
 
 
-class AddBookView(generic.ListView):
-    model = Book
-    queryset = Author.objects.all()
-    template_name = "blog/book.html"
+class BookDetail(View):
+    def get(self, request, book_id=-1):
+        context = {'object_list': Author.objects.all()}
+        if book_id >= 0:
+            queryset = Book.objects.filter(id=book_id)
+            context['book'] = get_object_or_404(queryset, id=book_id)
+        return render(request, "blog/book/book.html", context)
 
-
-def create_update_book(request, book_id=-1):
-    if request.method == "POST":
+    def post(self, request, book_id=-1):
         if book_id < 0:
             book_form = AddBookForm(data=request.POST)
         else:
@@ -31,22 +37,9 @@ def create_update_book(request, book_id=-1):
             book.reader = request.user
             book.save()
 
-        return HttpResponseRedirect(reverse('home'))
-
-    authors = Author.objects.all()
-    queryset = Book.objects.filter(id=book_id)
-    book = get_object_or_404(queryset, id=book_id)
-
-    return render(
-        request,
-        "blog/book.html",
-        {
-            "book": book,
-            "object_list": authors
-        },
-    )
+        return redirect('home')
 
 
-def delete_book(request, book_id):
-    get_object_or_404(Book.objects.all(), id=book_id).delete()
-    return HttpResponseRedirect(reverse('home'))
+class AuthorList(generic.ListView):
+    queryset = Author.objects.all()
+    template_name = "blog/author/index.html"
