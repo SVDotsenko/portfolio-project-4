803c646246eb68bfefed68329184295e05ecd10b
Serhii Dotsenko <serdotsenko@gmail.com>
2024-04-03 10:36:08 +0100

    The books were added into readers profile

--- reader/views.py
+++ reader/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import get_user
 from django.shortcuts import render, redirect
 from django.views import View
+from blog.models import Book
 
 
 class ReaderDetail(View):
     def get(self, request):
-        context = {'reader': get_user(request)}
+        reader = get_user(request)
+        books = Book.objects.filter(reader=reader)
+        context = {'reader': reader, 'books': books}
         return render(request, "reader/reader.html", context)
 
     def post(self, request):
