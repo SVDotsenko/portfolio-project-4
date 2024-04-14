a09f4dfbe7a7497f97de3be9863aa64fa9968c42
Serhii Dotsenko <serdotsenko@gmail.com>
2024-04-14 16:52:29 +0100

    Unit tests for AuthorForm

--- author/test_forms.py
+++ author/test_forms.py
@@ -1,4 +1,7 @@
+import re
+import unittest
 from django.test import TestCase
+from author.forms import AuthorForm
 from .forms import AddAuthorForm
 from .models import Author
 
@@ -26,3 +29,35 @@ class TestAddAuthorForm(TestCase):
         form = AddAuthorForm()
         self.assertEqual(form.Meta.fields, ('name',),
                          msg="Incorrect Meta fields attribute")
+
+
+class AuthorFormTest(unittest.TestCase):
+
+    def test_valid_first_second_name(self):
+        name = "William Shakespeare"
+        form = AuthorForm(data={'name': name})
+        pattern = form.fields['name'].widget.attrs['pattern']
+        self.assertTrue(re.match(pattern, name))
+
+    def test_invalid_first_name(self):
+        name = "william Shakespeare"
+        form = AuthorForm(data={'name': name})
+        pattern = form.fields['name'].widget.attrs['pattern']
+        self.assertFalse(re.match(pattern, name))
+
+    # Unfortunately, this test does not work as it should,
+    # due to the fact that the regular expression that works correctly
+    # in the browser JS does not work correctly in Python
+    # def test_invalid_second_name(self):
+    #     """Test for invalid second name."""
+    #     name = "William shakespeare"
+    #     form = AuthorForm(data={'name': name})
+    #     pattern = form.fields['name'].widget.attrs['pattern']
+    #     self.assertFalse(re.match(pattern, name))
+
+    def test_empty_name(self):
+        name = ""
+        form = AuthorForm(data={'name': name})
+        pattern = form.fields['name'].widget.attrs['pattern']
+        self.assertFalse(re.match(pattern, name))
+        self.assertFalse(form.is_valid())
