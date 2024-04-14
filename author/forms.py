da3e25a476a0d9e80ce9a14bc15f8511e997580b
Serhii Dotsenko <serdotsenko@gmail.com>
2024-04-14 14:36:21 +0100

    Unit tests for AddAuthorForm

--- author/forms.py
+++ author/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from author.models import Author
 
 
--- codestar/settings.py
+++ codestar/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -94,6 +94,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
