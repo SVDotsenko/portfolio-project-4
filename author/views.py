744464c237b799d83cf9d1413147cb61379a4536
Serhii Dotsenko <serdotsenko@gmail.com>
2024-04-17 17:30:14 +0100

    Refactoring: deleting author get to post method changed

--- author/views.py
+++ author/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-
 from author.forms import AddAuthorForm, AuthorForm
 from author.models import Author
 from book.models import Book
 
 
 class AuthorList(View):
-    def get_authors(self):
+    def get(self, request):
         books = Book.objects.all()
         authors = Author.objects.all().order_by('id')
         for author in authors:
             author.read = any(book.author == author and book.reader
                               for book in books)
-        return authors
+        return render(request, "author/authors.html", {'authors': authors})
 
-    def get(self, request, author_id=-1):
-        authors = self.get_authors()
-        if author_id < 0:
-            context = {'object_list': authors}
-            return render(request, "author/authors.html", context)
-        get_object_or_404(authors, id=author_id).delete()
+    def post(self, request, author_id):
+        get_object_or_404(Author, id=author_id).delete()
         return redirect('authors')
 
 
