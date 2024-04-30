0ca37ebf49a34b2e5c82e312e7b5871ec149b72d
Serhii Dotsenko <serdotsenko@gmail.com>
2024-04-30 17:49:06 +0100

    Updated place where element is placed

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,14 +1,4 @@
 const Toast = {
-    params: {
-        delay: 4,
-        autoHide: true,
-        messageTags: 'info',
-        message: 'Hello, world!'
-    },
-    paramToFunction: {
-        messageTags: messageTag => Toast.setColor(Toast.messageTagsMapping[messageTag]),
-        message: newMessage => Toast.setMessage(newMessage)
-    },
     messageTagsMapping: {
         debug: 'text-bg-secondary',
         info: 'text-bg-info',
@@ -16,19 +6,36 @@ const Toast = {
         warning: 'text-bg-warning',
         error: 'text-bg-danger'
     },
-    setMessage: message => Toast.params.element.querySelector('.toast-body').innerText = message,
-    removePreviousColorClass: () => Array.from(Toast.params.element.classList).forEach(className =>
-        className.startsWith('text-bg-') && Toast.params.element.classList.remove(className)),
+
+    params: {
+        messageTags: 'info',
+        message: 'Hello, world!',
+        delay: 4,
+        autoHide: true
+    },
+
+    paramsToFunction: {
+        messageTags: messageTag => Toast.setColor(Toast.messageTagsMapping[messageTag]),
+        message: newMessage => Toast.setMessage(newMessage)
+    },
+
+    setMessage: message => Toast.element.querySelector('.toast-body').innerText = message,
+
+    removePreviousColorClass: () => Array.from(Toast.element.classList).forEach(className =>
+        className.startsWith('text-bg-') && Toast.element.classList.remove(className)),
+
     setColor: newClass => {
         Toast.removePreviousColorClass();
-        Toast.params.element.classList.add(newClass);
+        Toast.element.classList.add(newClass);
     },
+
     processParameters: (o, f) => Object.keys(f).forEach(k => f[k](o[k])),
+
     show: newParams => {
         Toast.params = {...Toast.params, ...newParams};
-        Toast.processParameters(Toast.params, Toast.paramToFunction);
+        Toast.processParameters(Toast.params, Toast.paramsToFunction);
 
-        new bootstrap.Toast(Toast.params.element, {
+        new bootstrap.Toast(Toast.element, {
             animation: true,
             autohide: Toast.params.autoHide,
             delay: Toast.params.delay * 1000
