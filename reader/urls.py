c07f958cf218003207fd6022bfca17fa8fe02b08
Serhii Dotsenko <serdotsenko@gmail.com>
2024-05-03 11:05:55 +0100

    Revert "Update: temporarily removed verification of login to the application using login and password, for checking through online validators"
    This reverts commit c15cdd3496694f5bfd9ab2bb4b66753ef6a38d59.

--- reader/urls.py
+++ reader/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from .views import ProfileDetail
 
 urlpatterns = [
-    path('', ProfileDetail.as_view(), name='profile'),
+    path('', login_required(ProfileDetail.as_view()), name='profile'),
 ]
