c15cdd3496694f5bfd9ab2bb4b66753ef6a38d59
Serhii Dotsenko <serdotsenko@gmail.com>
2024-05-03 10:53:51 +0100

    Update: temporarily removed verification of login to the application using login and password, for checking through online validators

--- author/urls.py
+++ author/urls.py
@@ -1,14 +1,10 @@
-from django.contrib.auth.decorators import user_passes_test
 from django.urls import path
-from library.utils import is_admin
+
 from .views import AuthorList, AuthorDetail
 
 urlpatterns = [
-    path('', user_passes_test(is_admin, login_url='403')(AuthorList.as_view()),
-         name='authors'),
-    path('add/',
-         user_passes_test(is_admin, login_url='403')(AuthorDetail.as_view()),
-         name="add_author"),
+    path('', AuthorList.as_view(), name='authors'),
+    path('add/', AuthorDetail.as_view(), name="add_author"),
     path('update/<int:author_id>/', AuthorDetail.as_view(),
          name="update_author"),
     path('delete/<int:author_id>/', AuthorList.as_view(),
