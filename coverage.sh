e6de6fc8c66eddb508d5be17f937dd3d19b922f5
Serhii Dotsenko <serdotsenko@gmail.com>
2024-05-09 13:10:27 +0100

    Update: Added operating system check to select file opening command

--- coverage.sh
+++ coverage.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-echo -e "\033[41;37mRunning unit tests for Django\033[0m"
+open_report() {
+    case "$OSTYPE" in
+        "linux-gnu"*)
+            xdg-open $1
+            ;;
+        "darwin"*)
+            open $1
+            ;;
+        *)
+            start $1
+            ;;
+    esac
+}
+
+print_red() {
+    echo -e "\033[41;37m$1\033[0m"
+}
+
+print_red "Running unit tests for Django"
 rm -rf htmlcov
 coverage run --source='.' manage.py test
 coverage report -m
 coverage html
 rm .coverage
-start htmlcov/index.html
+open_report htmlcov/index.html
 
-echo -e "\033[41;37mRunning unit tests for JavaScript\033[0m"
+print_red "Running unit tests for JavaScript"
 rm -rf coverage
 npm run test
-start coverage/lcov-report/index.html
+open_report coverage/lcov-report/index.html
 
 cd htmlcov || exit
 rm .gitignore
